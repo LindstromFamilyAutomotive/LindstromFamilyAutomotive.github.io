664d4dc6f8b0b8c3f7a2a4bd9306423543a12623
lindstromfamilyautomotive <lindstromfamilyautomotive@gmail.com>
2016-06-09 14:33:48 -0400

    implemented scripts inline, removed need for jquery import

--- WebsiteOp2/scripts.js
+++ WebsiteOp2/scripts.js
@@ -1,34 +1,5 @@
-function initialize(){
-	var position = 0;
-	resizeScroller();
-	runScroll(position);
-	window.onresize = function(){resizeScroller();};
-	setInterval(function(){runScroll(position); position++;}, 5000);
-}
-
-function runScroll(position){
-	var scroller = document.getElementById("scroller1");
-
-	for(var i = 1; i < scroller.childNodes.length; i += 2)
-	{
-		scroller.childNodes[i].className = "a" + (((position + ((i+1)/2))%5)+1);
-	}
-}
-
-function resizeScroller(){
-	var topElement = document.getElementById("top");
-	var scroller = document.getElementById("scroller1");
-	
-	scroller.style.top = topElement.clientY + "px";
-	scroller.style.left = topElement.clientX + "px";
-	scroller.style.width = topElement.clientWidth + "px";
-	scroller.style.height = scroller.childNodes[1].offsetHeight + "px";
-	//scroller.style.clip = "rect(" + 0 + "px, " + topElement.clientWidth + "px, " + topElement.clientHeight + "px, " + 0 + "px)";
-	scroller.style.position = "relative"; 
-}
-
-function serviceScroll(location){
-	var parent = document.getElementById("servicesScrollContainer");
+function serviceScroll(location, parentId){
+	var parent = document.getElementById(parentId);
 	var child = parent.childNodes[location];
 	var speed = 30;
 	var currentState = 0;
